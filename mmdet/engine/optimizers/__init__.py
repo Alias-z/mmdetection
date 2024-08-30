e29be9c7189607d17a97c2513d7052db6ce08aa0
Hongyuan Zhang <66273343+Alias-z@users.noreply.github.com>
2024-08-30 11:29:55 +0200

    Added peft_optimizer

--- mmdet/engine/optimizers/__init__.py
+++ mmdet/engine/optimizers/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .layer_decay_optimizer_constructor import \
     LearningRateDecayOptimizerConstructor
+from .peft_optimizer import PEFTOptimWrapperConstructor
 
-__all__ = ['LearningRateDecayOptimizerConstructor']
+__all__ = ['LearningRateDecayOptimizerConstructor', 'PEFTOptimWrapperConstructor']
